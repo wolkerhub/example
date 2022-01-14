109823c471f96bf6c632c52104dbe63a20067f7b
vahid <vahid.muradov31@gmail.com>
2022-01-14 16:50:10 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using example.methods;
+Evvele:
+Console.WriteLine("1. Yola cix ");
+Console.WriteLine("2. Benzin doldur");
+Console.WriteLine("3. Yanacaq miqdari");
+Console.WriteLine("4. Yurus");
+Console.WriteLine("4. Cixis");
+
+
+string daxilEdilenReqem = Console.ReadLine();
+
+Go start = new Go();
+decimal benzin = 100m;
+
+
+switch (Convert.ToInt32(daxilEdilenReqem))
+{
+    case 1:
+        start.HereketeBasla(benzin);
+        goto Evvele;
+        break;
+      
+    case 2:
+        start.Benzindoldur(benzin);
+            goto Evvele;
+        break;
+    case 3:
+
+        break;
+    case 4:
+
+        break;
+    case 5:
+
+        break;
+
+    default:
+
+        break;
+}
---
+++ methods/Go.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example.methods
+{
+    public class Go
+    {
+        public void HereketeBasla(decimal benzin)
+        {
+            Console.WriteLine("Nece km yol gedeceksiniz?");
+
+            string yol = Console.ReadLine();
+
+            decimal netice = (Convert.ToDecimal(yol) * 10) / 100;
+
+            decimal qalanBenzin = benzin - netice;
+
+            if (qalanBenzin < 0)
+            {
+                Console.WriteLine( " Kiyafet qeder benzin yoxdur. " );
+            }
+
+            else
+            {
+                Console.WriteLine("Sizin " + " " + qalanBenzin + " litr benzininiz qaldi");
+
+            }
+
+
+
+        }
+
+        public void Benzindoldur(decimal benzin)
+        {
+            Console.WriteLine("Nece litr benzin doldurmaq isteyirsiniz?");
+            string Benzin = Console.ReadLine();
+
+
+            Console.WriteLine(Benzindoldur + " litr benzin doldurdunuz");
+
+        }
+    }
+}
